bab3e4d35e0e5c9086d06335d9289979815fbd74
Zaddish <40848785+Zaddish@users.noreply.github.com>
2025-03-10 22:02:27 +1100

    update vote panel details button

--- public/pages/ShowPost/components/VotesPanel.tsx
+++ public/pages/ShowPost/components/VotesPanel.tsx
@@ -17,8 +17,9 @@ export const VotesPanel = (props: VotesPanelProps) => {
   const fider = useFider()
   const [isVotesModalOpen, setIsVotesModalOpen] = useState(false)
   const canShowAll = fider.session.isAuthenticated && Fider.session.user.isCollaborator
-  const totalVotes = Math.abs(props.post.votesCount);
-  const extraVotesCount = totalVotes - props.votes.length;
+  // using negative here because even if the vote it negative itself, it's still a 'vote', 
+  // and as such, we need to show the total number of said votes.
+  const totalVotes = Math.abs(props.post.votesCount)
 
   const openModal = () => {
     if (canShowAll) {
@@ -36,22 +37,22 @@ export const VotesPanel = (props: VotesPanelProps) => {
           <Trans id="label.voters">Voters</Trans>
         </span>
       )}
-      {props.votes.length > 0 && (
-        <HStack spacing={0} className="gap-2">
-          <AvatarStack users={props.votes.map((x) => x.user)} overlap={false} />
-        </HStack>
-      )}
-      {extraVotesCount > 0 && (
-        <Button variant="tertiary" disabled={!canShowAll} size="small" onClick={openModal}>
-          <Trans id="showpost.votespanel.more">+{extraVotesCount} more</Trans>
-        </Button>
-      )}
-      {props.votes.length > 0 && extraVotesCount === 0 && canShowAll && (
-        <Button variant="tertiary" size="small" disabled={!canShowAll} onClick={openModal}>
-          <Trans id="showpost.votespanel.seedetails">see details</Trans>
-        </Button>
-      )}
-      {props.votes.length === 0 && (
+      {totalVotes > 0 ? (
+        <>
+          {props.votes.length > 0 && (
+            <HStack spacing={0} className="gap-2">
+              <AvatarStack users={props.votes.map((x) => x.user)} overlap={false} />
+            </HStack>
+          )}
+          {totalVotes > 1 ? (
+            <Button variant="tertiary" size="small" disabled={!canShowAll} onClick={openModal}>
+              {totalVotes} votes
+            </Button>
+          ) : (
+            <span>{totalVotes} vote</span>
+          )}
+        </>
+      ) : (
         <span className="text-muted">
           <Trans id="label.none">None</Trans>
         </span>
