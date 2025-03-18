d76803142cd63a9cfac8c338d31a05aecf30b624
Zaddish <40848785+Zaddish@users.noreply.github.com>
2025-03-18 16:11:02 +1100

    auto date update without refresh

--- public/components/common/Moment.tsx
+++ public/components/common/Moment.tsx
@@ -1,11 +1,11 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { classSet, formatDate, timeSince } from "@fider/services"
 
 interface MomentText {
   locale: string
   date: Date | string
   className?: string
-  format?: "relative" | "full" | "short" | "date"
+  format?: "relative" | "short" | "full" | "date"
 }
 
 export const Moment = (props: MomentText) => {
@@ -13,19 +13,59 @@ export const Moment = (props: MomentText) => {
     return <span />
   }
 
-  const format = props.format || "relative"
+  const [display, setDisplay] = useState<string>("")
+  const setNow = useState(new Date())[1]
 
-  const now = new Date()
+  const format = props.format || "relative"
   const date = props.date instanceof Date ? props.date : new Date(props.date)
-  const diff = (now.getTime() - date.getTime()) / (60 * 60 * 24 * 1000)
-  const display =
-    diff >= 365 && format === "relative"
-      ? formatDate(props.locale, props.date, "short")
-      : format === "relative"
-      ? timeSince(props.locale, now, date)
-      : format === "date"
-      ? formatDate(props.locale, props.date, "date")
-      : formatDate(props.locale, props.date, format)
+
+  const getTimeDiff = (): number => {
+    return (new Date().getTime() - date.getTime()) / 1000
+  }
+
+  const getRefreshInterval = (): number => {
+    const diffInSeconds = getTimeDiff()
+    
+    if (diffInSeconds < 60) {
+      return 1000
+    } else if (diffInSeconds < 3600) {
+      return 60 * 1000
+    } else if (diffInSeconds < 86400) {
+      return 60 * 60 * 1000
+    } else {
+      return 24 * 60 * 60 * 1000
+    }
+  }
+
+  const updateDisplayText = () => {
+    const currentNow = new Date()
+    const diffInDays = (currentNow.getTime() - date.getTime()) / (60 * 60 * 24 * 1000)
+    
+    const newDisplay =
+      diffInDays >= 365 && format === "relative"
+        ? formatDate(props.locale, props.date, "short")
+        : format === "relative"
+        ? timeSince(props.locale, currentNow, date)
+        : format === "date"
+        ? formatDate(props.locale, props.date, "date")
+        : formatDate(props.locale, props.date, format)
+    
+    setDisplay(newDisplay)
+    setNow(currentNow)
+  }
+
+  useEffect(() => {
+    updateDisplayText()
+    if (format === "relative") {
+      const interval = setInterval(() => {
+        updateDisplayText()
+      }, getRefreshInterval())
+
+      return () => clearInterval(interval)
+    }
+    
+    return undefined
+  }, [props.date, props.format, props.locale])
 
   const tooltip = props.format === "short" ? formatDate(props.locale, props.date, "full") : undefined
 
