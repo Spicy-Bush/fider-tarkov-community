26635a5ff6d198f5698c0bdb6c92b25040d5f0cc
Zaddish <40848785+Zaddish@users.noreply.github.com>
2025-12-10 14:59:28 +1100

    fix: remove watch mode from prod. oops

--- vite.config.ts
+++ vite.config.ts
@@ -24,11 +24,6 @@ export default defineConfig({
   build: {
     outDir: "dist",
     manifest: true,
-    watch: {
-      chokidar: {
-        usePolling: true,
-      },
-    },
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, "public/index.tsx"),
