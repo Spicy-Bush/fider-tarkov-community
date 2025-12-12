65cbd767d9a55bd24d48214bcff8d47a04bd3c76
Zaddish <40848785+Zaddish@users.noreply.github.com>
2025-12-12 11:45:07 +1100

    fix: dead code stopping build

--- public/components/app/index.ts
+++ public/components/app/index.ts
@@ -1,6 +1,6 @@
 export { Header } from "./Header"
 export { default as Footer } from "./Footer"
-export { NotificationIndicator, NotificationItem } from "./NotificationIndicator"
+export { NotificationIndicator } from "./NotificationIndicator"
 export { ReadOnlyNotice } from "./ReadOnlyNotice"
 export { ErrorBoundary } from "./ErrorBoundary"
 export { AdminPageLoader } from "./AdminPageLoader"
